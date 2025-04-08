02eb22fa099111196f2eaf81065678c783f57312
krithikravi <rcb44kr@hotmail.com>
2025-04-08 13:35:25 -0700

    first

---
+++ src/edu/upenn/cit5940/util/Area.java
@@ -0,0 +1,10 @@
+package edu.upenn.cit594.util;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Area {
+	String zipcode;
+	Integer population;
+	protected HashMap<String, Integer> vaccinations;
+	protected ArrayList<Property> properties;
+}
---
+++ src/edu/upenn/cit5940/util/Property.java
@@ -0,0 +1,15 @@
+package edu.upenn.cit594.util;
+public class Property {
+	Integer marketValue;
+	Integer livableArea;
+	public Property(Integer marketValue, Integer livableArea) {
+		this.marketValue = marketValue;
+		this.livableArea = livableArea;
+	}
+	public Integer getMarketValue() {
+		return marketValue;
+	}
+	public Integer getLivableArea() {
+		return livableArea;
+	}
+}
