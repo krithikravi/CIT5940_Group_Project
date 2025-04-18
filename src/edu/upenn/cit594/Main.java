6136e232753b7ded600d867e89f39582f7810b98
Zayd Ahmed <zayd.ahmed92@gmail.com>
2025-04-18 15:35:26 -0700

    Remove extra comments from Main class

--- src/edu/upenn/cit594/Main.java
+++ src/edu/upenn/cit594/Main.java
@@ -23,14 +23,7 @@ public class Main {
 		Set<String> argsTraversed = new HashSet<String>();
 		Logger logger = Logger.getInstance();
 		try {
-//			HashMap ret = new HashMap<Integer, Area>();
-//			Reader reader = Reader.getReader(args[0].split("=")[1], ret);;
-//			HashMap<Integer,Area> covid = reader.read();
-//			System.out.println(covid.keySet());
-//			System.out.println();
-//			System.out.println(covid.get(19102).getFullVaccinations().values());
-//			System.out.println(covid.get(19153).getPopulation());
-//			System.out.println(covid.get(19104).getProperties().getFirst().getLivableArea());
+
 //			if (args.length == 0 || args.length>4) {
 //				throw new IllegalArgumentException("Required: tweets_file states_file log_file");
 //			}
@@ -45,7 +38,6 @@ public class Main {
 			
 //			Check that all arguments are properly formatted
 			for (int i = 0; i<args.length; i++) {
-//				argsTraversed.clear(); //= new HashSet<String>(); 
 				String regex = "^--(?<name>.+?)=(?<value>.+)$";
 			    Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 			    if (!pattern.matcher(args[i]).find()) {
@@ -102,43 +94,18 @@ public class Main {
 			}
 			logger.log(sb.toString().strip());
 			
-//			    	 logger.loggerFileLocation(args[i]);
-			
-//			if (!args[0].startsWith("--population=")) {
-//				throw new IllegalArgumentException("Required argument format: --population=pop.csv"
-//			    			+ "--covid=cov.csv --properties=props.csv --log=log.txt");
-//			}
-//			if (!args[1].startsWith("--covid=")) {
-//				throw new IllegalArgumentException("Required argument format: --population=pop.csv"
-//			    			+ "--covid=cov.csv --properties=props.csv --log=log.txt");
-//			}
-//			if (!args[2].startsWith("--properties=")) {
-//				throw new IllegalArgumentException("Required argument format: --population=pop.csv"
-//			    			+ "--covid=cov.csv --properties=props.csv --log=log.txt");
-//			}
-//			if (!args[3].startsWith("--log=")) {
-//				throw new IllegalArgumentException("Required argument format: --population=pop.csv"
-//			    			+ "--covid=cov.csv --properties=props.csv --log=log.txt");
-//			}
-			
-			
-			
-			/* Need to check this
-			The format of the COVID data file can not be determined from the filename extension (“csv”
-			or “json”, case-insensitive). */
-//			if (!(args[2].toLowerCase().endsWith(".csv") || args[2].toLowerCase().endsWith(".json"))) {
-//				throw new IllegalArgumentException("covid file has incorrect extension");
-//			}
 
-//			HashMap ret = new HashMap<Integer, Area>();
+			
+			
+			
+
+
 			HashMap ret = new HashMap<Integer, Area>();
-//			Reader reader = Reader.getReader(args[0].split("=")[1], ret);;
-//			HashMap<Integer,Area> covid = reader.read();
+
 			
 			
 			
 			for (int i = 0; i<args.length; i++) {
-//				ret.clear();
 				if (!args[i].startsWith("--log")) {
 					String filename=args[i].split("=")[1];
 					Reader reader;
@@ -150,9 +117,7 @@ public class Main {
 					}
 					ret = (HashMap) reader.read().clone();
 					logger.log(args[i]);
-//					HashMap ret = new HashMap<Integer, Area>();
-//					Reader reader = Reader.getReader(args[0].split("=")[1], ret);;
-//					HashMap<Integer,Area> covid = reader.read();
+
 				}
 			}
 			
@@ -166,7 +131,6 @@ public class Main {
 		} catch (Exception e) {
 			System.err.println("Unexpected error: " + e.getMessage());
 		}
-//		UserInterface.printMenu();
 
 	}
 
